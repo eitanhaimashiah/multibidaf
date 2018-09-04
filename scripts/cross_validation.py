11949527f90e9a41de1b0c793e2d162ba1c44aa4
eitanhaimashiah <eitanhaimashiah@gmail.com>
2018-09-04 19:37:07 +0300

    Fix a bug in cross_validation.py

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -16,8 +16,6 @@ from multibidaf.models import MultipleBidirectionalAttentionFlow
 def grid_search(model_archive_path: str,
                 config_file: str,
                 serialization_dir: str,
-                overrides: str = "",
-                extend_vocab: bool = False,
                 file_friendly_logging: bool = False,
                 step: float = 0.2):
     thresholds = []
@@ -26,6 +24,10 @@ def grid_search(model_archive_path: str,
         for true_threshold in np.arange(0, 1, step):
             for false_threshold in np.arange(0, true_threshold + step, step):
                 thresholds.append((span_threshold, true_threshold, false_threshold))
+                print("#" * 100)
+                print("The current setting is (span_threshold={}, true_threshold={}, false_threshold={})"
+                      .format(span_threshold, true_threshold, false_threshold))
+                print("#" * 100)
 
                 # Delete the serialization directory, if exists.
                 if os.path.exists(serialization_dir) and os.listdir(serialization_dir):
@@ -45,8 +47,8 @@ def grid_search(model_archive_path: str,
                                 file_friendly_logging)
 
                 # Add the best validation F1_m score of this run to f1_m_scores
-                ext_vars = dict(os.environ)
-                metrics = json.loads(os.path.join(serialization_dir, "metrics.json"), ext_vars=ext_vars)
+                with open(os.path.join(serialization_dir, "metrics.json")) as f:
+                    metrics = json.load(f)
                 f1_m_scores.append(metrics["best_validation_f1_m"])
 
     # Find the best setting
@@ -54,10 +56,10 @@ def grid_search(model_archive_path: str,
     argmax = f1_m_scores.index(max(f1_m_scores))
     best_setting = thresholds[argmax]
 
-    print("#" * 50)
+    print("#" * 100)
     print("The best setting is (span_threshold={}, true_threshold={}, false_threshold={}) with F1_m={}"
           .format(best_setting[0], best_setting[1], best_setting[2], max_f1_m))
-    print("#" * 50)
+    print("#" * 100)
 
 
 if __name__ == "__main__":
@@ -65,3 +67,9 @@ if __name__ == "__main__":
                 "training_config/multirc_config.json",
                 "/tmp/multirc_serialization",
                 step=0.2)
+
+    # grid_search("/tmp/test/squad_serialization/model.tar.gz",
+    #             "multibidaf/tests/fixtures/multirc_experiment.json",
+    #             "/tmp/test/multirc_serialization",
+    #             step=0.2)
+
