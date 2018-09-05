7baa47c5466eeafbd4bcb0bf6b5b72c518d25549
eitanhaimashiah <eitanhaimashiah@gmail.com>
2018-09-05 10:21:26 +0300

    Cross-validation process by hand (0.7, 0.6, 0.6)

--- multibidaf/models/multibidaf.py
+++ multibidaf/models/multibidaf.py
@@ -97,8 +97,8 @@ class MultipleBidirectionalAttentionFlow(BidirectionalAttentionFlow):
                  regularizer: Optional[RegularizerApplicator] = None,
                  tfidf_path: str = None,
                  span_threshold: float = 0.7,
-                 true_threshold: float = 0.4,
-                 false_threshold: float = 0.4) -> None:
+                 true_threshold: float = 0.6,
+                 false_threshold: float = 0.6) -> None:
 
         self._is_squad = False
         span_end_encoder = span_end_encoder or Seq2SeqEncoder.from_params(Params({"type": "lstm",
